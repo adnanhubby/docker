179da0be0fbb6dbe1b9ebcce169e12024925027b
root <root@ip-172-31-21-233.ec2.internal>
2022-03-16 20:53:40 +0000

    project

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import flask
+import os
+import sys
+app = Flask(__name__)
+
+app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+if __name__ == '__main__':
+    username = os.getenv("USERNAME")
+    if username == None or username == "":
+        print("please specify the username SECRET")
+        sys.exit(1)
+    app.run(debug=True, host='0.0.0.0')
+
+~
